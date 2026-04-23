2eb82d42e78bf5bf752bb136563a3e6103af0b23
baljeetgunghas2 <jaatmrharyanvi@gmail.com>
2026-04-23 22:06:45 +0530

    two pointer on shorted array

--- week1/23-04-2026.js
+++ week1/23-04-2026.js
@@ -5,3 +5,26 @@
 //         console.log(i);
 //     }, 1000);
 // }
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+
+function twoSumSorted(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+  // since we have shorted array so we have idea we have to increase left or decrease right
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === target) {
+      return [left, right];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return null;
+}
+
+console.log(twoSumSorted(nums, target)); // Expected: [0, 1]
