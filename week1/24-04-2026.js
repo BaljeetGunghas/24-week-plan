d3a888a7b757fca2533074743f0838fa380d9345
baljeetgunghas2 <jaatmrharyanvi@gmail.com>
2026-04-24 22:03:09 +0530

    sumrange with recursion

---
+++ week1/24-04-2026.js
@@ -0,0 +1,17 @@
+// Sum of N Numbers
+
+// Example: sumRange(3) should return 6 ($3 + 2 + 1$).
+// Example: sumRange(5) should return 15 ($5 + 4 + 3 + 2 + 1$).
+
+// function sumrange(num) {
+//   // if number is equal to 1 than i have to stop this rescursion fun
+//   // this is the main track for recursion if we missed this condition over call stack will overflow and excude memorey
+//   if (num === 1) {
+//     return 1;
+//   }
+
+//   return num + sumrange(num - 1);
+// }
+
+// console.log(sumrange(3));
+// console.log(sumrange(5));
